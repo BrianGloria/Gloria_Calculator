864f6324883528effba2d9672ded2e6cfed244b8
Brian <bribriangloria@gmail.com>
2022-03-02 21:44:58 +0700

    added functionality

--- app/src/main/java/mcm/edu/ph/gloria_calculator/MainActivity.java
+++ app/src/main/java/mcm/edu/ph/gloria_calculator/MainActivity.java
@@ -3,12 +3,74 @@ package mcm.edu.ph.gloria_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+abstract class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText txtVariable1, txtVariable2;
+    Button btnAdd, btnSubtract, btnMultiply, btnDivide;
+    TextView txtAnswer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        txtVariable1 = findViewById(R.id.txtVariable1);
+        txtVariable2 = findViewById(R.id.txtVariable2);
+
+        btnAdd=findViewById(R.id.btnAdd);
+        btnSubtract=findViewById(R.id.btnSubtract);
+        btnMultiply=findViewById(R.id.btnMultiply);
+        btnDivide=findViewById(R.id.btnDivide);
+
+        txtAnswer=findViewById(R.id.textAnswer);
+
+
+
+        btnAdd.setOnClickListener(this);
+        btnMultiply.setOnClickListener(this);
+        btnSubtract.setOnClickListener(this);
+        btnDivide.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        double variable1, variable2, answer;
+
+        variable1 = Double.parseDouble(String.valueOf(txtVariable1.getText()));
+        variable2 = Double.parseDouble(String.valueOf(txtVariable2.getText()));
+
+
+
+
+        switch (v.getId()) {
+
+            case R.id.btnAdd:
+                answer = variable1 + variable2;
+                txtAnswer.setText(String.valueOf(answer));
+                break;
+            case R.id.btnSubtract:
+                answer = variable1 - variable2;
+                txtAnswer.setText(String.valueOf(answer));
+                break;
+            case R.id.btnDivide:
+                answer = variable1 / variable2;
+                txtAnswer.setText(String.valueOf(answer));
+                break;
+            case R.id.btnMultiply:
+                answer = variable1 * variable2;
+                txtAnswer.setText(String.valueOf(answer));
+                break;
+
+        }
     }
 }
